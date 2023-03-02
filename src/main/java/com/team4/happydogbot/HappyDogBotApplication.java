c84cf51e0593e8b43810266f019b70f1ba8d7890
TamaraZolotovskaya <tamarafx25@gmail.com>
2023-03-02 18:08:07 +0700

    Пустой Spring Boot проект

---
+++ src/main/java/com/team4/happydogbot/HappyDogBotApplication.java
@@ -0,0 +1,13 @@
+package com.team4.happydogbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyDogBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyDogBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team4/happydogbot/HappyDogBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4.happydogbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyDogBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
