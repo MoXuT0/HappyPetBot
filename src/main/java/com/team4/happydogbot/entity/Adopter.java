6d44d5c75bb52962c0aa4421b450062526d9b870
salva <salvationordeath@gmail.com>
2023-03-12 19:59:14 +0700

    Обновила entity, подключила бд - таблицы созданы

--- src/main/java/com/team4/happydogbot/entity/Adopter.java
+++ src/main/java/com/team4/happydogbot/entity/Adopter.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 /**
  * Class describing the user(adopter)
- *
+ * @param 'status' a field to display the level of user interaction (displays the stage or state the user is in)
  */
 @Data
 @NoArgsConstructor
@@ -27,13 +27,26 @@ public class Adopter {
     private String firstName;
     @Column(name = "last_name", nullable = false, length = 25)
     private String lastName;
+    @Column(name = "user_name", nullable = false)
+    private String userName;
     @Column(name = "age")
     private int age;
+    @Column(name = "address", nullable = false, length = 50)
+    private String address;
     @Column(name = "phone_number", nullable = false, length = 15)
     private String telephoneNumber;
+    //поле для отображения уровня взаимодействия с пользователем
+    //(отображает этап или состояние, в котором находится пользователь)
     @Column(name = "status", nullable = false, length = 15)
     @Enumerated(EnumType.STRING)
-    Enum<Status> state;
+    Status state;
+
     @OneToMany(mappedBy = "adopter", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Report> reports;
+
+    public Adopter(Long chatId, String userName, Status state) {
+        this.chatId = chatId;
+        this.userName = userName;
+        this.state = state;
+    }
 }
--- src/main/java/com/team4/happydogbot/entity/Report.java
+++ src/main/java/com/team4/happydogbot/entity/Report.java
@@ -4,12 +4,15 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.telegram.telegrambots.meta.api.objects.File;
+
 
 import java.time.LocalDate;
 
 /**
  * Class describing the users(adopters) report on the state of the dog
- *
+ * @param 'examination' field for the verification status (performed by a volunteer) of the report (default - null)
  */
 @Data
 @NoArgsConstructor
@@ -21,13 +24,22 @@ public class Report {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "report_id")
     private long id;
-    @Column(name = "chat_id", nullable = false)
-    private Long chatId;
-    @Column(name = "report_text", nullable = false)
-    private String reportText;
+//    @Column(name = "chat_id", nullable = false)
+//    private Long chatId;
+    //@CreationTimestamp
     @Column(name = "report_date")
     private LocalDate reportDate;
     //поля для обработки фотографий
+    private byte[] picture;
+    private File filePicture;
+    private String filePath;
+    //поле текста при добавлении фото
+    @Column(name = "report_text", nullable = false)
+    private String caption;
+    //поле для статуса проверки(выполняется волонтером) отчета (по умолчанию - null)
+    @Column(name = "examination")
+    private Boolean examination;
+
     @ManyToOne
     @JoinColumn(name = "chat_id")
     private Adopter adopter;
