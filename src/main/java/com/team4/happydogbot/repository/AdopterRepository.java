24e04ee981020c554e70f55dc50fc380ca767a0b
salva <salvationordeath@gmail.com>
2023-03-17 20:27:07 +0700

    Переделала для Cat: entity, repository, service и controller. Добавила пакет с exceptions, создала классы для Cat.

--- src/main/java/com/team4/happydogbot/repository/AdopterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team4.happydogbot.repository;
-
-import com.team4.happydogbot.model.Adopter;
-import com.team4.happydogbot.entity.Adopter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdopterRepository extends JpaRepository<Adopter, Long> {
-    Adopter findAdopterByChatId(Long id);
-}
